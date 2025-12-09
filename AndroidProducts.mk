650df92403cca1ce68085914a07d0e02a151de3a
Your Name <you@example.com>
2025-12-09 12:04:00 +0530

    initial: generic device tree for air (mt6835)

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_air-user \
+    omni_air-userdebug \
+    omni_air-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo omni_air-user
+add_lunch_combo omni_air-userdebug
+add_lunch_combo omni_air-eng
